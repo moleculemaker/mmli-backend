b95f5f48442e7185d8794943c5c88dc6b7dc9b89
Samarth Gupta <samarthgupta@Samarths-MacBook-Air.local>
2023-10-23 23:57:40 -0500

    Improved error message for /analyze db failure and added flagging molecule APIs

--- app/routers/chemscraper.py
+++ app/routers/chemscraper.py
@@ -9,6 +9,7 @@ from services.minio_service import MinIOService
 from services.chemscraper_service import ChemScraperService
 
 from models.analyzeRequestBody import AnalyzeRequestBody
+from models.sqlmodel.models import FlaggedMolecule, FlaggedMoleculeDelete
 
 from sqlmodel.ext.asyncio.session import AsyncSession
 from models.sqlmodel.db import get_session
@@ -24,6 +25,7 @@ async def analyze_documents(requestBody: AnalyzeRequestBody, background_tasks: B
     if len(requestBody.fileList) > 0 and requestBody.jobId != "":
         # Create a new job and add to the DB
         separator = "|"
+        curr_time = time.time()
         db_job = Job(
             email=requestBody.user_email,
             job_info=separator.join(requestBody.fileList),
@@ -31,21 +33,53 @@ async def analyze_documents(requestBody: AnalyzeRequestBody, background_tasks: B
             # Run ID takes the default value since the app doesn't support multiple runs right now
             type=JobType.CHEMSCRAPER,
             user_agent='',
-            time_created=int(time.time())
+            time_created=int(curr_time)
         )
 
         try: 
             db.add(db_job)
             await db.commit()
-        except:
-            content = {"jobId": requestBody.jobId, "error_message": "Job ID already exists with the same ID. Please retry using another job ID."}
+        except Exception as e:
+            content = {"jobId": requestBody.jobId, "error_message": "Database Error Occured.", "error_details": str(e)}
             return JSONResponse(content=content, status_code=400) 
 
         filename = requestBody.fileList[0]
         chemscraperService = ChemScraperService(db=db)
         objectPath = f"inputs/{requestBody.jobId}/{filename}"
         background_tasks.add_task(chemscraperService.runChemscraperOnDocument, 'chemscraper', filename, objectPath, requestBody.jobId, service)
-        content = {"jobId": requestBody.jobId, "submitted_at": datetime.now().isoformat()}
+        content = {"jobId": requestBody.jobId, "submitted_at": datetime.fromtimestamp(curr_time).isoformat()}
         return JSONResponse(content=content, status_code=status.HTTP_202_ACCEPTED) 
 
+@router.post("/chemscraper/flag", tags=['ChemScraper'])
+async def flag_molecule(requestBody: FlaggedMolecule, db: AsyncSession = Depends(get_session)):
+    flagged_molecule = FlaggedMolecule(
+        smile=requestBody.smile,
+        job_id=requestBody.job_id,
+        doc_id=requestBody.doc_id,
+        time_created=int(time.time())
+    )
+    try:
+        db.add(flagged_molecule)
+        await db.commit()
+    except Exception as e:
+            content = {"jobId": requestBody.job_id, "error_message": "Unable to flag molecule. Database Error Occured.", "error_details": str(e)}
+            return JSONResponse(content=content, status_code=400) 
+    
+    content = {"success_message": "Molecule Flag Successful"}
+    return JSONResponse(content=content, status_code=status.HTTP_202_ACCEPTED) 
 
+@router.delete("/chemscraper/flag", tags=['ChemScraper'])
+async def delete_flagged_molecule(requestBody: FlaggedMoleculeDelete, db: AsyncSession = Depends(get_session)):
+    flagged_molecule = db.get(FlaggedMolecule, (requestBody.smile, requestBody.job_id))
+    try: 
+        if flagged_molecule:
+            db.delete(flagged_molecule)
+            await db.commit()
+        else:
+            raise Exception("Flagged Molecule with Job ID and SMILE representation Not Found.")
+    except Exception as e:
+                content = {"jobId": requestBody.job_id, "error_message": "Unable to delete flagged molecule. Database Error Occured.", "error_details": str(e)}
+                return JSONResponse(content=content, status_code=400) 
+         
+    content = {"success_message": "Molecule Flag Successful"}
+    return JSONResponse(content=content, status_code=status.HTTP_202_ACCEPTED) 
