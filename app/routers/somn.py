dfa041c097e0c78a5e127428aa2d7376af329d25
ckouder <bingjiguo@icloud.com>
2024-05-06 12:13:12 -0500

    add reaction sites selection endpoint

--- app/routers/somn.py
+++ app/routers/somn.py
@@ -16,6 +16,7 @@ from models.somnRequestBody import SomnRequestBody
 from models.sqlmodel.db import get_session
 from models.sqlmodel.models import Job, JobType
 
+from rdkit import Chem
 
 router = APIRouter()
 
@@ -52,3 +53,35 @@ async def start_somn(
     )
     content = {"jobId": requestBody.jobId, "submitted_at": datetime.now().isoformat()}
     return JSONResponse(content=content, status_code=status.HTTP_202_ACCEPTED)
+
+
+@router.get(f"/{JobType.SOMN}/all-reaction-sites", tags=['Somn'])
+async def check_reaction_sites(smiles: str, role: str):
+    reactionSiteIdxes = SomnService.check_user_input_substrates(smiles, role)
+
+    mol = Chem.MolFromSmiles(smiles)
+    d2d = Chem.Draw.rdMolDraw2D.MolDraw2DSVG(300, 150)
+
+    # Settings for SVG Image coloring via draw options
+    dopts = d2d.drawOptions()
+    dopts.continuousHighlight=False
+    dopts.highlightBondWidthMultiplier = 24
+    dopts.highlightRadius = .4
+    dopts.setHighlightColour((.9,.9,0))
+    dopts.prepareMolsBeforeDrawing=True
+    dopts.fillHighlights=False
+
+    def get_highlight_atom_color(idxes):
+        return { idx: (0, 0, 0, 0) for idx in idxes }
+
+    Chem.rdDepictor.Compute2DCoords(mol)
+
+    d2d.DrawMolecule(mol, 
+                     highlightAtoms=reactionSiteIdxes, 
+                     highlightAtomColors=get_highlight_atom_color(reactionSiteIdxes))
+    d2d.FinishDrawing()
+
+    return {
+        "reaction_site_idxes": reactionSiteIdxes,
+        "svg": d2d.GetDrawingText()
+    }
--- app/services/somn_service.py
+++ app/services/somn_service.py
@@ -14,6 +14,9 @@ from models.enums import JobStatus
 from services.minio_service import MinIOService
 from services.email_service import EmailService
 
+from typing import List
+from rdkit import Chem
+
 class SomnService:
     somn_frontend_baseURL = os.environ.get("SOMN_FRONTEND_URL")
 
@@ -95,4 +98,94 @@ class SomnService:
             retVal.append(data)
             
         return retVal
-        
+    
+    @staticmethod
+    def check_user_input_substrates(user_input, role):
+        """
+        Verifies user input substrate, and if verification is successful, returns reaction sites if multiple.
+        If everything is "normal", i.e., the user doesn't need to tell us more information, then it returns 0.
+        If NONE are found for halides or reactive nitrogens, then this returns None.
+        """
+
+        def get_amine_ref_ns(mol, ref_atom_idxes: List) -> List:
+            """
+            Returns the reference atom index for the nitrogen most likely to be reactive, or multiple (if there are multiple)
+            """
+            
+            reactive_nitrogens = []
+            for idx in ref_atom_idxes:
+                neighbors = mol.GetAtomWithIdx(idx).GetNeighbors()
+                n_hs = [1 for nbr in neighbors if nbr.GetSymbol() == "H"]
+                reactive_nitrogens.append(sum(n_hs))
+
+            reactive_nitrogens = np.array(reactive_nitrogens)
+            atm_indices = np.where((reactive_nitrogens == np.max(reactive_nitrogens)) & (reactive_nitrogens != 0))[0]
+
+            if len(atm_indices) >= 1:
+                return atm_indices
+            
+            raise Exception("No reactive nitrogens detected in nucleophile!")
+            
+        def check_halides_aromatic(rdkmol,halides):
+            rdkatoms = [atom for atom in rdkmol.GetAtoms() if atom.GetIdx() in halides]
+
+            outIdxes = []
+            for atom in rdkatoms:
+                nbrs = atom.GetNeighbors()
+                assert len(nbrs) == 1
+                if nbrs[0].GetIsAromatic() is True: 
+                    outIdxes.append(atom.GetIdx())
+
+            return outIdxes
+
+        def get_atoms_by_symbol(mol, symbol):
+            num_atoms = mol.GetNumAtoms()
+            retVals = []
+            for idx in range(num_atoms):
+                atom = mol.GetAtomWithIdx(idx)
+                if atom.GetSymbol() == symbol:
+                    retVals.append(idx)
+            return retVals
+
+        mol = Chem.MolFromSmiles(user_input)
+        mol = Chem.AddHs(mol)
+
+        bromides = get_atoms_by_symbol(mol, symbol="Br")
+        chlorides = get_atoms_by_symbol(mol, symbol="Cl")
+        nitrogens = get_atoms_by_symbol(mol, symbol="N")
+
+        if role == 'nucleophile':
+            import numpy as np
+            if len(bromides) != 0:
+                aromatic_halides = check_halides_aromatic(mol,bromides)
+                if any(idx in aromatic_halides for idx in bromides):
+                    raise Exception("Bromides detected in nucleophile!")
+
+            if len(chlorides) != 0:
+                aromatic_halides = check_halides_aromatic(mol,bromides)
+                if any(idx in aromatic_halides for idx in chlorides):
+                    raise Exception("Chlorides detected in nucleophile!")
+            
+            if len(nitrogens) == 0:
+                raise Exception("No nitrogens detected in nucleophile!")
+            
+            indices = get_amine_ref_ns(mol,nitrogens)
+            return indices
+
+        elif role == 'electrophile':
+            if len(bromides) + len(chlorides) == 0:
+                raise Exception("No Br or Cl sites detected in electrophile!")
+            
+            try:
+                chl_idxes = check_halides_aromatic(mol,chlorides)
+                brm_idxes = check_halides_aromatic(mol,bromides)
+
+                if len(brm_idxes + chl_idxes) == 0: 
+                    return None
+                
+                return chl_idxes + brm_idxes
+            
+            except Exception as e:
+                raise e
+
+        return None
