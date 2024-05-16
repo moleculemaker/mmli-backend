ccc17c62c853d6ead41c8b4de07c076ba58ff749
ckouder <bingjiguo@icloud.com>
2024-05-16 15:25:43 -0500

    fix: add opststoic result parsing

--- app/services/novostoic_service.py
+++ app/services/novostoic_service.py
@@ -1,20 +1,30 @@
 import asyncio
 import os
 import time
+import json
+import re
+
+from fastapi import HTTPException
 
 from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlmodel import select
+from sqlalchemy import or_
 
-from models.sqlmodel.models import Job
+from models.sqlmodel.models import Job, ChemicalIdentifier
 from models.enums import JobStatus
 
 from services.minio_service import MinIOService
 from services.email_service import EmailService
+
+from rdkit.Chem import CanonSmiles
 
 class NovostoicService:
     novostoic_frontend_baseURL = os.environ.get("NOVOSTOIC_FRONTEND_URL")
 
     def __init__(self, db) -> None:
         self.db = db
+        
+
 
     async def update_job_phase(self, jobObject, phase: JobStatus):
         jobObject.phase = phase
@@ -76,8 +86,63 @@ class NovostoicService:
         return await self.runOptstoic(bucket_name, payload, service, email_service) 
     
     @staticmethod
+    async def getChemicalByMetanetXId(metanetx_id: str, db: AsyncSession):
+        chemical = (await db.execute(select(ChemicalIdentifier).where(ChemicalIdentifier.metanetx_id == metanetx_id))).all()
+        return chemical[0][0] if chemical else None
+    
+    @staticmethod
     async def optstoicResultPostProcess(bucket_name: str, job_id: str, service: MinIOService, db: AsyncSession):
-        return service.get_file(bucket_name, f"results/{job_id}/{job_id}.csv")
+        # get job info
+        job = await db.get(Job, job_id)
+        if not job:
+            raise HTTPException(status_code=404, detail="Job not found")
+        
+        job_info = json.loads(job.job_info)
+        primary_precursor = await NovostoicService.getChemicalByMetanetXId(job_info['primary_precursor'], db)
+        target_molecule = await NovostoicService.getChemicalByMetanetXId(job_info['target_molecule'], db)
+        
+        # get all files of the job
+        start_idx = 1
+        file_name = f"results/{job_id}/metanetx_id/soln_dict_metanetx_id{start_idx}.json"
+        files = []
+        while service.check_file_exists(bucket_name, file_name):
+            file = service.get_file(bucket_name, file_name)
+            files.append(json.loads(file))
+            start_idx += 1
+            file_name = f"results/{job_id}/metanetx_id/soln_dict_metanetx_id{start_idx}.json"
+            
+        reactions = []
+        for file in files:
+            reactants = []
+            products = []
+            for key in file:
+                if key == 'dG_Range' or \
+                    key == target_molecule.metanetx_id or \
+                    key == primary_precursor.metanetx_id:
+                    continue
+                
+                y = int(file[key])
+                target = reactants if y < 0 else products
+                chemical = await NovostoicService.getChemicalByMetanetXId(key, db)
+                target.append({
+                    "molecule": chemical if chemical else key,
+                    "amount": abs(y)
+                })
+            deltaG = re.search(r"dG range = (-?\d+)", file['dG_Range'])
+            reactions.append({
+                "stoichiometry": {
+                    "reactants": reactants,
+                    "products": products,
+                },
+                "yield": file[target_molecule.metanetx_id],
+                "deltaG": deltaG.group(1) if deltaG else None,
+            })
+            
+        return {
+            "primaryPrecursor": primary_precursor,
+            "targetMolecule": target_molecule,
+            "results": reactions
+        }
     
     @staticmethod
     async def novostoicResultPostProcess(bucket_name: str, job_id: str, service: MinIOService, db: AsyncSession):
