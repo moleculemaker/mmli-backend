cfe91fad5f41841eddd3f894e42ebfd4784a2317
ckouder <bingjiguo@icloud.com>
2024-05-29 10:01:19 -0500

    remove redundant functions

--- app/routers/novostoic.py
+++ app/routers/novostoic.py
@@ -1,22 +1,14 @@
-import time
-from datetime import datetime
 from typing import Optional, Union
 
-from fastapi import APIRouter, Depends, BackgroundTasks, status
-from fastapi.responses import JSONResponse
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from sqlalchemy import or_
 
-from services.minio_service import MinIOService
-from services.email_service import EmailService
-from services.novostoic_service import NovostoicService
-
-from sqlmodel import select, func
+from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
 
-from models.novostoicRequestBodies import DgPredictorRequestBody, NovostoicRequestBody, EnzRankRequestBody, OptstoicRequestBody
 from models.sqlmodel.db import get_session
-from models.sqlmodel.models import Job, JobType, ChemicalIdentifier
+from models.sqlmodel.models import ChemicalIdentifier
 
 from rdkit.Chem import CanonSmiles
 
@@ -30,40 +22,6 @@ class ChemicalAutoCompleteResponse(BaseModel):
     structure: Optional[str]
 
 router = APIRouter()
-
-@router.post(f"/{JobType.NOVOSTOIC_OPTSTOIC}/run", tags=['Novostoic'])
-async def start_optstoic(
-    requestBody: OptstoicRequestBody, 
-    background_tasks: BackgroundTasks, 
-    service: MinIOService = Depends(), 
-    db: AsyncSession = Depends(get_session), 
-    email_service: EmailService = Depends()
-):
-    #ASSUMPTION: a job of novostoic/optstoic has been created in the db already
-    if requestBody.jobId == "":
-        content = {"jobId": requestBody.jobId, "error_message": "Job ID not provided."}
-        return JSONResponse(content=content, status_code=400)
-    
-    existing_job = await db.execute(select(Job).where(Job.job_id == requestBody.jobId))
-    if not existing_job.first():
-        content = {"jobId": requestBody.jobId, "error_message": "Job not found."}
-        return JSONResponse(content=content, status_code=404)
-
-    #TODO: add validation for the input
-    payload = {
-        "job_id": requestBody.jobId,
-    }
-
-    novostoicService = NovostoicService(db=db)
-    background_tasks.add_task(
-        novostoicService.runOptstoic, 
-        JobType.NOVOSTOIC_OPTSTOIC,
-        payload,
-        service, 
-        email_service
-    )
-    content = {"jobId": requestBody.jobId, "submitted_at": datetime.now().isoformat()}
-    return JSONResponse(content=content, status_code=status.HTTP_202_ACCEPTED)
 
 @router.get(f"/chemical/auto-complete", 
             tags=['Novostoic'], 
@@ -134,107 +92,3 @@ async def validate_chemical(search: str, db: AsyncSession = Depends(get_session)
         "metanetx_id": chemical[4],
         "kegg_id": chemical[5]
     }
-
-@router.post(f"/{JobType.NOVOSTOIC_PATHWAYS}/run", tags=['Novostoic'])
-async def start_novostoic(
-    requestBody: NovostoicRequestBody, 
-    background_tasks: BackgroundTasks, 
-    service: MinIOService = Depends(), 
-    db: AsyncSession = Depends(get_session), 
-    email_service: EmailService = Depends()
-):
-    #ASSUMPTION: a job of novostoic/optstoic has been created in the db already
-    if requestBody.jobId == "":
-        content = {"jobId": requestBody.jobId, "error_message": "Job ID not provided."}
-        return JSONResponse(content=content, status_code=400)
-    
-    existing_job = await db.execute(select(Job).where(Job.job_id == requestBody.jobId))
-    if not existing_job.first():
-        content = {"jobId": requestBody.jobId, "error_message": "Job not found."}
-        return JSONResponse(content=content, status_code=404)
-
-    #TODO: add validation for the input
-    payload = {
-        "job_id": requestBody.jobId,
-    }
-
-    novostoicService = NovostoicService(db=db)
-    background_tasks.add_task(
-        novostoicService.runNovostoic, 
-        JobType.NOVOSTOIC_PATHWAYS,
-        payload,
-        service, 
-        email_service
-    )
-    content = {"jobId": requestBody.jobId, "submitted_at": datetime.now().isoformat()}
-    return JSONResponse(content=content, status_code=status.HTTP_202_ACCEPTED)
-
-
-@router.post(f"/{JobType.NOVOSTOIC_ENZRANK}/run", tags=['Novostoic'])
-async def start_enzrank(
-    requestBody: EnzRankRequestBody, 
-    background_tasks: BackgroundTasks, 
-    service: MinIOService = Depends(), 
-    db: AsyncSession = Depends(get_session), 
-    email_service: EmailService = Depends()
-):
-    #ASSUMPTION: a job of novostoic/optstoic has been created in the db already
-    if requestBody.jobId == "":
-        content = {"jobId": requestBody.jobId, "error_message": "Job ID not provided."}
-        return JSONResponse(content=content, status_code=400)
-    
-    existing_job = await db.execute(select(Job).where(Job.job_id == requestBody.jobId))
-    if not existing_job.first():
-        content = {"jobId": requestBody.jobId, "error_message": "Job not found."}
-        return JSONResponse(content=content, status_code=404)
-
-    #TODO: add validation for the input
-    payload = {
-        "job_id": requestBody.jobId,
-    }
-
-    novostoicService = NovostoicService(db=db)
-    background_tasks.add_task(
-        novostoicService.runEnzRank, 
-        JobType.NOVOSTOIC_ENZRANK,
-        payload,
-        service, 
-        email_service
-    )
-    content = {"jobId": requestBody.jobId, "submitted_at": datetime.now().isoformat()}
-    return JSONResponse(content=content, status_code=status.HTTP_202_ACCEPTED)
-
-
-@router.post(f"/{JobType.NOVOSTOIC_DGPREDICTOR}/run", tags=['Novostoic'])
-async def start_dgpredictor(
-    requestBody: DgPredictorRequestBody, 
-    background_tasks: BackgroundTasks, 
-    service: MinIOService = Depends(), 
-    db: AsyncSession = Depends(get_session), 
-    email_service: EmailService = Depends()
-):
-    #ASSUMPTION: a job of novostoic/optstoic has been created in the db already
-    if requestBody.jobId == "":
-        content = {"jobId": requestBody.jobId, "error_message": "Job ID not provided."}
-        return JSONResponse(content=content, status_code=400)
-    
-    existing_job = await db.execute(select(Job).where(Job.job_id == requestBody.jobId))
-    if not existing_job.first():
-        content = {"jobId": requestBody.jobId, "error_message": "Job not found."}
-        return JSONResponse(content=content, status_code=404)
-
-    #TODO: add validation for the input
-    payload = {
-        "job_id": requestBody.jobId,
-    }
-
-    novostoicService = NovostoicService(db=db)
-    background_tasks.add_task(
-        novostoicService.runDgPredictor, 
-        JobType.NOVOSTOIC_DGPREDICTOR,
-        payload,
-        service, 
-        email_service
-    )
-    content = {"jobId": requestBody.jobId, "submitted_at": datetime.now().isoformat()}
-    return JSONResponse(content=content, status_code=status.HTTP_202_ACCEPTED)
