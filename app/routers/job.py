70b0acac2e0185942d4a43cfaec5304072ea8ad3
Sara Lambert <lambert8@illinois.edu>
2024-07-15 11:04:38 -0500

    feat: upgrade to use SOMN 1.5 image

--- app/routers/job.py
+++ app/routers/job.py
@@ -101,7 +101,8 @@ async def create_job(
                 raise HTTPException(status_code=400, detail="Failed to upload file to MinIO")
 
             # We assume that file has already been uploaded to MinIO
-            somn_project_dir = '/tmp/somn_root/somn_scratch/last'
+            #somn_project_dir = '/tmp/somn_root/somn_scratch/last'
+            somn_project_dir = '/tmp/somn_root/somn_scratch/IID-Models-2024'
             #input_file_path = f'{somn_project_dir}/scratch/example_request.csv'
             #output_file_path = f'{somn_project_dir}/outputs/asdf'
 
