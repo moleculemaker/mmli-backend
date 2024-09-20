61975bb72ca82bf076c93d2de8ca1af44ebb1520
Kastan Day <kastanvday@gmail.com>
2024-09-20 19:07:53 +0000

    Further cleanup

--- app/services/aceretro_service.py
+++ app/services/aceretro_service.py
@@ -1,37 +1,16 @@
-from typing import List
 from fastapi import HTTPException
-import requests
-import io
-import os
-import csv
-import zipfile
 import time
-import pandas as pd
-from io import StringIO
 
-from config import get_logger, app_config
-from models.sqlmodel.models import Job
+from config import get_logger
 from models.enums import JobStatus
 
 from services.minio_service import MinIOService
-from services.rdkit_service import RDKitService
-from services.pubchem_service import PubChemService
-from services.email_service import EmailService
-from models.molecule import Molecule
-from fastapi import Depends
-from fastapi.responses import JSONResponse
 
-from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
-
-from models.sqlmodel.models import FlaggedMolecule
 
 log = get_logger(__name__)
 
 class ACERetroService:
-    chemscraper_api_baseURL = app_config['external']['chemscraper']['apiBaseUrl']
-    chemscraper_frontend_baseURL = app_config['external']['chemscraper']['frontendBaseUrl']
-
     def __init__(self, db) -> None:
         self.db = db
     
@@ -46,7 +25,10 @@ class ACERetroService:
 
     @staticmethod
     async def resultPostProcess(bucket_name: str, job_id: str, service: MinIOService, db: AsyncSession):
-        # rdkitService = RDKitService()
+        """
+        Inputs stored in Minio:  /{job_id}/in/input.json    Bucket name: aceretro
+        Outputs stored in Minio: /{job_id}/out/output.json  Bucket name: aceretro
+        """
         filepath = f"/{job_id}/out/output.json"
         content = service.get_file(bucket_name, filepath)
         if content is None:
