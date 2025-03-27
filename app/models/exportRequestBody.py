a648594d4aebc66952d916bad6f678bc236af391
Sara Lambert <lambert8@illinois.edu>
2025-03-27 18:30:54 -0500

    fix: adjust ChemScraper export paths and similarity sort

--- app/models/exportRequestBody.py
+++ app/models/exportRequestBody.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 
 class ExportRequestBody(BaseModel):
     jobId: str
+    pdf_filename: str
     cdxml: bool
     cdxml_filter: str
     cdxml_selected_pages: List[int]
