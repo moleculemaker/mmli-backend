33813a82046dacd00199b9100d39a5bd5548db94
ckouder <bingjiguo@icloud.com>
2025-05-09 11:31:42 -0500

    fix smiles with highlight index

--- app/routers/shared.py
+++ app/routers/shared.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter, HTTPException
+from typing import Annotated
+from fastapi import APIRouter, HTTPException, Query
 from services.shared import draw_chemical_svg
 
 router = APIRouter()
 
 @router.get("/smiles/draw", tags=['Shared'])
-async def draw_smiles(smiles: str, highlightAtoms: list[int] = []):
+async def draw_smiles(
+    smiles: str, 
+    highlightAtoms: Annotated[list[int] | None, Query()] = Query(default=None)
+):
     if type(smiles) != str: 
         raise HTTPException(status_code=400, detail=f"Input must be a single SMILES string. Got type: `{type(smiles)}` with SMILES = `{smiles}`")
     
