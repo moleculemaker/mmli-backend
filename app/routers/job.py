3a480a6e94239c40e5bf2b94729ebbab5d6e0f12
Sara Lambert <lambert8@illinois.edu>
2025-06-09 19:50:44 -0500

    fix: unescape double-quotes for correct JSON syntax

--- app/routers/job.py
+++ app/routers/job.py
@@ -131,7 +131,7 @@ async def create_job(
         elif job_type == JobType.OED_DLKCAT or job_type == JobType.OED_UNIKP or job_type == JobType.OED_CATPRED:
             # Example: "job_info": "{\"input_pairs\":[{\"name\":\"example\",\"sequence\":\"MEDIPDTSRPPLKYVK...\",\"type\":\"FASTA\",\"smiles\":\"OC1=CC=C(C[C@@H](C(O)=O)N)C=C1\"}]}"
             log.debug(f'Running OpenEnzymeDB job: {job_type} - {job_id}')
-            environment = [{'name': 'OED_JOB_INFO', 'value': json.dumps(job_info)}]
+            environment = [{'name': 'OED_JOB_INFO', 'value': json.dumps(job_info.replace('\\"', '"'))}]
 
         elif job_type == JobType.SOMN:
             #  Build up example_request.csv from user input, upload to MinIO?
