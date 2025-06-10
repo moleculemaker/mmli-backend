e192a8f12f7bdb8ef39b51339976c52a2e71b633
Sara Lambert <lambert8@illinois.edu>
2025-06-09 21:00:03 -0500

    fix: one more time

--- app/routers/job.py
+++ app/routers/job.py
@@ -131,9 +131,13 @@ async def create_job(
         elif job_type == JobType.OED_DLKCAT or job_type == JobType.OED_UNIKP or job_type == JobType.OED_CATPRED:
             # Example: "job_info": "{\"input_pairs\":[{\"name\":\"example\",\"sequence\":\"MEDIPDTSRPPLKYVK...\",\"type\":\"FASTA\",\"smiles\":\"OC1=CC=C(C[C@@H](C(O)=O)N)C=C1\"}]}"
             log.debug(f'Running OpenEnzymeDB job: {job_type} - {job_id}')
+            log.debug(f'    job_info: {job_info}')
             json_str = job_info.replace('\"', '"')
+            log.debug(f'    json_str: {json_str}')
             json_obj = json.loads(json_str)
+            log.debug(f'    json_obj: {json_obj}')
             environment = [{'name': 'OED_JOB_INFO', 'value': json.dumps(json_obj)}]
+            log.debug(f'    environment: {environment}')
 
         elif job_type == JobType.SOMN:
             #  Build up example_request.csv from user input, upload to MinIO?
