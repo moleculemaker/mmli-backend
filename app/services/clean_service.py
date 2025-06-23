43f9319f37e4ef3eca0ee73bb778ebfd7e8b2a1d
Sara Lambert <lambert8@illinois.edu>
2025-06-23 18:44:25 -0500

    fix: correct the /results endpoint for cleandb-mepesm

--- app/services/clean_service.py
+++ app/services/clean_service.py
@@ -93,23 +93,14 @@ class CleanService:
         """
         Outputs stored in Minio: /{job_id}/out/*  Bucket name: oed-*
         """
-        folder_path = f"/{job_id}/out/"
-        objects = service.list_files(bucket_name, folder_path)
+        folder_path = f"/{job_id}/out"
 
         # Iterate over folder and add all contents to a dictionary
-        content = {}
-        for obj in objects:
-            file_name = os.path.basename(obj.object_name).split('/')[-1]
-            if file_name.endswith('.json'):
-                content[file_name] = json.loads(service.get_file(bucket_name=bucket_name, object_name=obj.object_name))
-            elif file_name.endswith('.csv'):
-                content[file_name] = service.get_file(bucket_name=bucket_name, object_name=obj.object_name)
-            else:
-                log.warning(f'Skipping unrecognized file extension: ' + str(file_name))
-
-        # Return the dictionary if it has contents
-        if not content:
-            raise HTTPException(status_code=404, detail=f"No output files were found")
+        try:
+            json_contents = service.get_file(bucket_name=bucket_name, object_name=f'{folder_path}/mepesm-output.json')
+            return json.loads(json_contents)
+        except Exception as e:
+            raise HTTPException(status_code=404, detail=f"Failed to fetch output file: {str(e)}")
 
 
     @staticmethod
